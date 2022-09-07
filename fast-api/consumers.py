dc14bdf9c7a4de181bdfd1de53c80faf1e0468df
Anfrian <gorkemkadir14@hotmail.com>
2022-09-07 16:18:04 +0300

    api update

--- fast-api/consumers.py
+++ fast-api/consumers.py
@@ -20,6 +20,8 @@ def start_delivery(state, event):
 def pickup_products(state,event):
     data = json.loads(event.data)
     new_budget = state["budget"] - int(data['purchase_price'])*int(data["quantity"])
+    if new_budget < 0:
+        raise HTTPException(status_code=400, detail = "Budget not enough")
     return state | {
         "budget": new_budget,
         "purchase_price":int(data['purchase_price']),
@@ -29,8 +31,11 @@ def pickup_products(state,event):
 
 def deliver_products(state,event):
     data = json.loads(event.data)
-    new_budget = state["budget"] + int(data['sell_price']) * int(data["quantity"])
-    new_quantity = state["quantity"] - int(data['quantity'])
+    new_budget = state["budget"] + int(data["sell_price"]) * int(data["quantity"])
+    new_quantity = state["quantity"] - int(data["quantity"])
+    if new_quantity < 0:
+        raise HTTPException(status_code=400, detail = "Quantity not enough")
+
     return state | {
         "budget": new_budget,
         "sell_price": int(data['sell_price']),
@@ -38,10 +43,17 @@ def deliver_products(state,event):
         "status": "collected"
     }
 
+def increase_budget(state,event):
+    data = json.loads(event.data)
+    state['budget'] += int(data['budget'])
+    return state
+
 
 CONSUMERS = {
     "CREATE_DELIVERY": create_delivery,
     "START_DELIVERY": start_delivery,
     "PICKUP_PRODUCTS": pickup_products,
     "DELIVER_PRODUCTS": deliver_products,
+    "INCREASE_BUDGET" : increase_budget,
+
 }
--- fast-api/main.py
+++ fast-api/main.py
@@ -42,7 +42,19 @@ async def get_state(pk: str):
     state = redis.get(f'delivery:{pk}')
     if state is not None:
         return json.loads(state)
-    return {}
+    state = build_state(pk)
+    resis.set(f'delivery:{pk}', json.dumps(state))
+    return state
+
+
+def build_state(pk:str):
+    pks = Event.all_pks()
+    all_events = [Event.get(pk) for pk in pks]
+    events = [event for event in all_events if event.delivery_id == pk]
+    state = {}
+    for event in events:
+        state = consumers.CONSUMERS[event.type](state, event)
+    return state
 
 @app.post('/deliveries/create')
 async def create(request: Request):
